d623114aab5b90949e07e50e4894253e32cc82af
Andrea Piai <andrea.piai@pagopa.it>
2024-02-27 11:44:08 +0100

    Http for Android

--- android/src/main/java/com/pagopa/ioreactnativehttpclient/IoReactNativeHttpClientModule.kt
+++ android/src/main/java/com/pagopa/ioreactnativehttpclient/IoReactNativeHttpClientModule.kt
@@ -4,6 +4,8 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class IoReactNativeHttpClientModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -15,8 +17,18 @@ class IoReactNativeHttpClientModule(reactContext: ReactApplicationContext) :
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  fun multiply(a: String, promise: Promise) {
+    var client = OkHttpClient()
+    val request: Request = Request.Builder()
+      .url(a)
+      .build()
+    client.newCall(request).execute().use { response ->
+      if (response.isSuccessful) {
+        promise.resolve(response.body!!.string())
+      } else {
+        promise.resolve("Error")
+      }
+    }
   }
 
   companion object {
