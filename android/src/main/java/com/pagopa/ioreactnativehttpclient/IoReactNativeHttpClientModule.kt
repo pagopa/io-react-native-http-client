e5462bb5df748dcdea509ee6c1cdfe120c4aaa3e
Andrea Piai <andrea.piai@pagopa.it>
2024-05-25 00:09:11 +0200

    WiP

--- android/src/main/java/com/pagopa/ioreactnativehttpclient/IoReactNativeHttpClientModule.kt
+++ android/src/main/java/com/pagopa/ioreactnativehttpclient/IoReactNativeHttpClientModule.kt
@@ -5,8 +5,12 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReadableMap
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import com.facebook.react.bridge.ReadableNativeMap
+import com.facebook.react.bridge.WritableNativeMap
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.http.HttpMethod
+import kotlinx.coroutines.CoroutineScope
 
 class IoReactNativeHttpClientModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
@@ -15,9 +19,66 @@ class IoReactNativeHttpClientModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
+  private val runningCoroutines = HashMap<String, CoroutineScope>()
+  private var client: HttpClient? = null;
+
+  private fun initializeClientIfNeeded() {
+    if (client == null) {
+      client = HttpClient(Android)
+    }
+  }
+
+  private fun deallocate() {
+    cancelAllRunningRequests()
+    client?.close()
+    client = null
+  }
+
   @ReactMethod
   fun nativeRequest(config: ReadableMap, promise: Promise) {
+    initializeClientIfNeeded()
+    val verb = stringFromConfigForKey(config, "verb") ?: run {
+      handleNonHttpFailure("Bad configuration, missing 'verb'", promise)
+      return
+    }
+
+    val methodOpt = optMethodFromVerb(verb) ?: run {
+      handleNonHttpFailure("Unsupported configuration, unknown verb '$verb'", promise)
+      return
+    }
+
+    val url = stringFromConfigForKey(config, "url") ?: run {
+      handleNonHttpFailure("Bad configuration, missing 'url'", promise)
+      return
+    }
+
     // TODO
+  }
+
+  private fun stringFromConfigForKey(configOpt: ReadableMap?, key: String): String? =
+    configOpt?.let {
+      return try {
+        it.getString(key)
+      } catch (e: Exception) {
+        null
+      }
+    }
+
+
+  private fun optMethodFromVerb(verb: String): HttpMethod? =
+    if ("get".equals(verb, ignoreCase = true)) HttpMethod.Get else if ("post".equals(
+        verb,
+        ignoreCase = true
+      )
+    ) HttpMethod.Post else null
+
+  fun handleNonHttpFailure(message: String, promise: Promise) {
+    val failureHttpResponse = WritableNativeMap()
+    failureHttpResponse.putString("type", "failure")
+    failureHttpResponse.putInt("code", 900)
+    failureHttpResponse.putString("message", message)
+    failureHttpResponse.putMap("headers", WritableNativeMap())
+    promise.resolve(failureHttpResponse)
   }
 
   @ReactMethod
